a5cd3f81c1aa253882a80acca39b5ca1c5250797
glowacz <glowacki.pj@gmail.com>
2025-11-16 13:31:06 +0100

    [feat]: lab6 skeleton

---
+++ dslab06/solution.rs
@@ -0,0 +1,28 @@
+use std::path::PathBuf;
+// You can add here other imports from std or crates listed in Cargo.toml.
+
+// You can add any private types, structs, consts, functions, methods, etc., you need.
+// As always, you should not modify the public interfaces.
+
+#[async_trait::async_trait]
+pub trait StableStorage: Send + Sync {
+    /// Stores `value` under `key`.
+    ///
+    /// Detailed requirements are specified in the description of the assignment.
+    async fn put(&mut self, key: &str, value: &[u8]) -> Result<(), String>;
+
+    /// Retrieves value stored under `key`.
+    ///
+    /// Detailed requirements are specified in the description of the assignment.
+    async fn get(&self, key: &str) -> Option<Vec<u8>>;
+
+    /// Removes `key` and the value stored under it.
+    ///
+    /// Detailed requirements are specified in the description of the assignment.
+    async fn remove(&mut self, key: &str) -> bool;
+}
+
+/// Creates a new instance of stable storage.
+pub async fn build_stable_storage(root_storage_dir: PathBuf) -> Box<dyn StableStorage> {
+    unimplemented!()
+}
